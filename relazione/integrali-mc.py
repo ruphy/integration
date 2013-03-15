417bbc3327df665d05540facf27ccb352ad8f781
Riccardo Iaconelli <riccardo@kde.org>
2013-03-15 10:05:09 +0100

    other fixes
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- relazione/integrali-mc.py
+++ relazione/integrali-mc.py
@@ -1,24 +1,22 @@
 
 import matplotlib.pyplot as plt
 
-#execfile("mc-out.py")
-#execfile('deltamc.py')
-execfile('build/deltamc2.py')
+execfile('data/deltamc-10-10.py')
 
 x = []
 y = []
 
-for i in a:
+for i in dati:
     x.append(i[0])
     y.append(abs(i[1]))
 
 plt.clf()
-plt.plot(x, y, '.-')
+plt.plot(x, y, 'o')
 plt.yscale('log')
 plt.grid(True)
 plt.xscale('log')
 plt.xlabel('Numero di campionamenti')
 plt.ylabel('Errore (modulo)')
+plt.title(r"Metodo Montecarlo $\int_1^{10} (e^x+1+x^9-8x^8+\sinh(5x))e^{-x^2}dx$")
 
-plt.savefig('images/outmc.png')
-
+plt.savefig('images/montecarlo-err.png')
