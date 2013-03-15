5950369721df88d0390f7daa194c31187ed8257e
Riccardo Iaconelli <riccardo@kde.org>
2013-03-15 15:26:55 +0100

    make it faster
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- integrali/integralemc.cpp
+++ integrali/integralemc.cpp
@@ -51,7 +51,7 @@ IntegraleMC::IntegraleMC(double a, double b)
 //         std::cout << "[" <<  i << ", " << run() - 417.8077704440582 << "]," << std::endl;
 //     }
 
-    for (int i = 100; i < pow(10, 8); i += 100) {
+    for (int i = 1000; i < pow(10, 8); i += 1000) {
         m_n = i;
         statRun();
     }
