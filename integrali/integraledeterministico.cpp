7869917c6fa1c4c0686498b0d67f34ef91378d41
Riccardo Iaconelli <riccardo@kde.org>
2013-03-17 14:14:11 +0100

    more statistics
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- integrali/integraledeterministico.cpp
+++ integrali/integraledeterministico.cpp
@@ -50,7 +50,7 @@ IntegraleDeterministico::IntegraleDeterministico(int a, int b)
     std::cout.precision(std::numeric_limits<double>::digits10 + 1);
 
     // Statistica
-    for (int i = 0; i <= 1000; i++) {
+    for (int i = 0; i <= 100000; i=(i+1)*log(i)) {
         setIntervalli(i);
         std::cout << "[" << i << ", " << trapezi() << ", " << simpson() << ", " << gauss() << "]," << std::endl;
     }
