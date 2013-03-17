f1a3d7604370ddc29631881a4782cf19814c4055
Riccardo Iaconelli <riccardo@kde.org>
2013-03-17 10:31:27 +0100

    add more statistical analysis
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- relazione/integrali-mc.py
+++ relazione/integrali-mc.py
@@ -1,5 +1,6 @@
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 execfile('data/deltamc-10-10.py')
 
@@ -20,3 +21,32 @@ plt.ylabel('Errore (modulo)')
 plt.title(r"Metodo Montecarlo $\int_1^{10} (e^x+1+x^9-8x^8+\sinh(5x))e^{-x^2}dx$")
 
 plt.savefig('images/montecarlo-err.png')
+
+execfile('data/mc-stat-10-8.py')
+x = []
+y = []
+errors = []
+
+for i in dati:
+    x.append(i[0])
+    y.append(abs(i[1]-417.8077704440582))
+    errors.append(i[2])
+
+# Mantieni gli errori positivi (log plot)
+y = np.array(y)
+errors = np.array(errors)
+#ylower = np.maximum(1e-4, y - errors)
+#yerr_lower = y - ylower
+ylower = np.zeros_like(y)
+yerr_lower = ylower
+
+plt.clf()
+plt.errorbar(x, y, xerr=0, yerr=[yerr_lower, 2*errors], fmt='.') #xerr=errors, fmt='.')
+plt.yscale('log')
+plt.grid(True)
+plt.xscale('log')
+plt.xlabel('Numero di campionamenti')
+plt.ylabel('Errore medio (modulo)')
+plt.title(r"Metodo Montecarlo $\int_1^{10} (e^x+1+x^9-8x^8+\sinh(5x))e^{-x^2}dx$")
+
+plt.savefig('images/montecarlo-con-stat.png')
